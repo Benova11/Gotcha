e04e8d52f517daf8995314b59b8b370d83fe9423
Benova <ben.afr@gmail.com>
2021-06-19 16:04:48 +0300

    added road prefabs varients

--- Gotcha/Assets/Enemy/EnemyMover.cs
+++ Gotcha/Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;;
+    [SerializeField] float waitTime = 1f;
 
     void Start()
     {
