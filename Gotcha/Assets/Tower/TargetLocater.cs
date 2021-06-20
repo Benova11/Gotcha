5eccf3e9a4469da2e68be067c929f2d3c0c38289
Benova <ben.afr@gmail.com>
2021-06-20 21:06:06 +0300

    added ballista logic, attack closest in range enemy

--- Gotcha/Assets/Tower/TargetLocater.cs
+++ Gotcha/Assets/Tower/TargetLocater.cs
@@ -6,21 +6,45 @@ public class TargetLocater : MonoBehaviour
 {
 
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectilleParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
-
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
     void AimWeapon()
     {
-        weapon.transform.LookAt(target);
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
+        Attack(targetDistance <= range);
+
+        weapon.LookAt(target);
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
+    void Attack(bool isInRange)
+    {
+        var emissionModule = projectilleParticles.emission;
+        emissionModule.enabled = isInRange;
     }
 }
