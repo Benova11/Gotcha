5f5284b416924fee45c733f7b4bc8a2c1f30252f
Benova <ben.afr@gmail.com>
2021-06-25 14:35:46 +0300

    added path finding init tools, opening new branch

---
+++ Gotcha/Assets/PathFinding/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node
+{
+    public Node parentNode;
+    public Vector2Int coordinates;
+    public bool isPassable;
+    public bool isExplored;
+    public bool isPath;
+
+    public Node(Vector2Int coordinates, bool isPassable)
+    {
+        this.coordinates = coordinates;
+        this.isPassable = isPassable;
+    }
+}
